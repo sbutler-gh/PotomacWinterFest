a52cb9f8bf3848bbff8bda72b3824e6359cb0a13
sbutler-gh <sam@ysli.be>
2023-12-03 12:20:48 -0500

    first commit

---
+++ functions/postVolunteer.js
@@ -0,0 +1,73 @@
+// could always just return the jsondata and then parse?
+
+// adding counts to sections
+
+require('dotenv').config();
+const querystring = require('querystring');
+// import fetch from 'node-fetch';
+const fetch = require('node-fetch')
+const { Headers } = fetch;
+
+
+exports.handler = async (event) => {
+
+    const form = querystring.parse(event.body);
+
+    let interests = [];
+    console.log(form);
+
+
+for (const key in form) {
+    if (form[key] == "on") {
+        interests.push(key);
+    }
+  }
+
+  console.log(interests);
+
+    // for (let i = 0; i < form.length; i++) {
+    //     console.log(form[i]);
+    //     // if (form[i] == "on") {
+    //     //     interests.push(form[i]);
+    //     // }
+    // }
+
+    let records = {
+        "records": [
+          {
+            "fields": {
+              "email": form.email,
+            //   "interests": JSON?.stringify(interests),
+              "location": form.location,
+              "details": form.details,
+            }
+          }
+        ]
+      }
+
+    console.log(records);
+
+    if (event.path != "/favicon.ico") {
+        const response = await fetch(`https://docs.getgrist.com/api/docs/${process.env.GRIST_DOCUMENT_ID}/tables/Table1/records`, { 
+            method: 'POST',
+            body: JSON.stringify(records), 
+            headers: new Headers({
+                'Authorization': `Bearer ${process.env.GRIST_API_KEY}`,
+                "Content-Type": "application/json"
+            })
+        });
+                
+        const res = await response.json();
+        console.log(res);
+
+        let success = `<p style="color: green; font-weight: 500; font-style: italic;">RSVP successful!  Now share this site and invite others to join!  https://pss.pm/dec16</p>`;
+
+          return {
+            statusCode: 200,
+            headers: {
+                'Content-type': 'text/html; charset=UTF-8',
+              },
+              body: success,
+            }
+    }
+}
